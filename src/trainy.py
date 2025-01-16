294cd7ad4ca00ba6b2f9446b26eb9b0b677d22bd
sailaja2090 <sailu2090@gmail.com>
2025-01-16 18:10:00 +0530

    Added feature

--- src/trainy.py
+++ src/trainy.py
@@ -2,6 +2,7 @@ import sys
 import os
 # Dynamically add the project root to Python's search path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.model import create_model
 from src.utils import load_data
 from sklearn.model_selection import train_test_split
