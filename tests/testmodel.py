7591d717e710bc89cc2746b5a3ca78de4841177d
sailaja2090 <sailu2090@gmail.com>
2025-01-17 17:00:39 +0530

    Added feature

--- tests/testmodel.py
+++ tests/testmodel.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.model import create_model
 
-
 def test_create_model():
     model = create_model()
     assert model is not None, "Model should not be None"
